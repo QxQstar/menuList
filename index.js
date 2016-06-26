4aad3ce32b5482c4467b022287ae9781464c4322
QxQstar <1395294694@qq.com>
2016-06-26 19:48:06 +0800

    加了注释

--- index.js
+++ index.js
@@ -27,10 +27,13 @@ var untilEvent = {
 	},
 	getRelated:function(event){
 		if(event.relatedTarget){
+			//兼容DOM的浏览器将相关元素保持在relatedTarget属性中
 			return event.relatedTarget;
 		}else if(event.toElement){
+			//在IE浏览器中mouseout事件的相关元素保存在toElement属性中
 			return event.toElement;
 		}else if(event.fromElement){
+			//在IE浏览器中mouseover事件的相关元素保持在fromElement属性中
 			return event.fromElement;
 		}else{
 			return null;
@@ -38,27 +41,31 @@ var untilEvent = {
 	}
 
 };
+//下面这四个元素用于表示四个定时器的标识，最开始我只使用两个定时器，当快速移动时
+//动画会乱。
 var timeDec1,timeAdd1,timeAdd2,timeDec2;
 function getOuter(){
 	var outerList1 = document.getElementById('outerList1');
 	var outerList2 = document.getElementById('outerList2');
-	untilEvent.addEvent(outerList1,'mouseover',callBackOver);
+	var outer = document.getElementById('outer');
+	untilEvent.addEvent(outer,'mouseover',callBackOver);
 	untilEvent.addEvent(outerList1,'mouseout',callBackOut1);
-	untilEvent.addEvent(outerList2,'mouseover',callBackOver);
 	untilEvent.addEvent(outerList2,'mouseout',callBackOut2);
 }
+//mouseout事件：当鼠标从一个元素移入另一个元素时在鼠标离开的那个元素
+//上触发，获得鼠标的元素可能在失去鼠标元素的外部也可能在失去鼠标元素的
+//内部.所以需要判断mouseout事件的相关元素是否为外部li（即id为outerList或id为outerList2）元素
+//的子孙元素，如果是子孙元素，则内部无序列表无须收起。
 function callBackOut1(event){
 	var event = untilEvent.getEvent(event);
 	var relatedTarget = untilEvent.getRelated(event);
 	var outerList1 = document.getElementById('outerList1');
-	var outerList2 = document.getElementById('outerList2');
 	var inter1 = document.getElementById('inter1');
-	var inter2 = document.getElementById('inter2');
 	var flag = false;
 	if(relatedTarget !== null){
 		var parented = relatedTarget.parentNode;
 		do{
-			if(parented === outerList1){
+			if(parented === outerList1 || relatedTarget === outerList1){
 				flag = true;
 				break;
 			}else{
@@ -79,7 +86,7 @@ function callBackOut2(event){
 	if(relatedTarget !== null){
 		var parented = relatedTarget.parentNode;
 		do{
-			if(parented === outerList2){
+			if(parented === outerList2 || relatedTarget === outerList2){
 				flag = true;
 				break;
 			}else{
@@ -99,6 +106,8 @@ function changeHeightDec1(element){
 		var height = parseInt(element.style.height);
 		if(height > 0){
 			if(height%offHeight){
+				//当鼠标快速移动，确保height能够被offheight整除
+				//如果不能整除，内部ul的高度可能不能减小到零
 				height = height-height%offHeight;
 			}
 			if(height >= offHeight){
@@ -112,6 +121,10 @@ function changeHeightDec1(element){
 }
 function changeHeightDec2(element){
 	var offHeight = 70;
+	//清除定时器，确保当快速移动鼠标时，动画的流畅性
+	//例如：当鼠标移入后，在内部ul的长度还没有变为最大值时，又移除鼠标
+	//清除定时器可以保证，鼠标移除后内部ul的长度立即减小。timeAdd2或的timeAdd1
+	//是控制内部ul长度增加的定时器
 	clearTimeout(timeAdd2);
 	change2();
 	function change2(){
@@ -132,23 +145,21 @@ function changeHeightDec2(element){
 function callBackOver(event){
 	var event = untilEvent.getEvent(event);
 	var target = untilEvent.getTarget(event);
-	var relatedTarget = untilEvent.getRelated(event);
-	var outerList1 = document.getElementById('outerList1');
-	var outerList2 = document.getElementById('outerList2');
-	var inter1,inter2;
+	var inter1 = document.getElementById('inter1');
+	var inter2 = document.getElementById('inter2');
 	console.log(target.id);
 	if(target.id == 'outerList1' || target.id == "link1"){
-		inter1 = document.getElementById('inter1');
 		changeHeight(inter1,target);
 	}
 	if(target.id == 'outerList2' || target.id == 'link2'){
-		inter2 = document.getElementById('inter2');
 		changeHeight(inter2,target);
 	}
 }
 function changeHeight(element,tar){
 	var totalHeight = 200;
 	if(tar.id == 'outerList1' || tar.id == 'link1'){
+		//当鼠标移入时清除让内部ul长度减小的定时器，保证鼠标移入后
+		//内部ul长度立即增加
 		clearTimeout(timeDec1);
 		change1();
 	}
